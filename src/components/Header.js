f9c1ad78479b10c6ae5b621e8df637ed8f69332a
Andres Hadechine <andreshadechine@gmail.com>
2021-12-01 19:54:38 -0500

    Creacion de proyecto inicial

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+    return (
+      <div className="header">
+          <figure>
+              <img src="../public/logo.png" alt="" />
+          </figure>
+        <h1>Todo List</h1>
+      </div>
+    );
+  };
+  
+  export default Header;

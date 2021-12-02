c32c2464342a6fddfaf379fb3b17ee9637d8a850
AndresHadechine <andres3g.1@gmail.com>
2021-12-02 15:01:16 -0500

    definicion de estructura de los movimientos

--- src/components/ListMove.js
+++ src/components/ListMove.js
@@ -1,45 +1,45 @@
 import React from "react";
-import './ListMove.css';
+import "./ListMove.css";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Form from "react-bootstrap/Form";
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Row from "react-bootstrap/Row";
+import Movements from "./Movements";
 
 const ListMove = () => {
   return (
     <Modal.Dialog>
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title>Listado Movimientos</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-      <div>
-        <Form>
-        <Form.Group
+        <div>
+          <Form>
+            <Form.Group
               as={Row}
               md="4"
               controlId="validationFormik101"
               className="position-relative"
             >
-            <InputGroup hasValidation>
-          <InputGroup.Text ><FontAwesomeIcon icon={faSearch} /></InputGroup.Text>
-          <Form.Control
-                type="text"
-                name="firstName"
-              />
-
-          <Form.Check type="radio" label="Todos" name="selection" id />
-          <Form.Check type="radio" label="Ingreso" name="selection" id />
-          <Form.Check type="radio" label="Gasto" name="selection" id />
-          </InputGroup>
-          </Form.Group>
-        </Form>
-      </div>
+              <InputGroup>
+                <InputGroup.Text>
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+                <Form.Control type="text" name="firstName" />
+                <Form.Check type="radio" label="Todos" name="selection" id />
+                <Form.Check type="radio" label="Ingreso" name="selection" id />
+                <Form.Check type="radio" label="Gasto" name="selection" id />
+              </InputGroup>
+            </Form.Group>
+          </Form>
+        </div>
+        <br />
+        <div>
+          <Movements />
+        </div>
       </Modal.Body>
     </Modal.Dialog>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,54 +1,55 @@
-import React from 'react'
-import './Register.css'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFile, faDollarSign } from '@fortawesome/free-solid-svg-icons'
-
-
+import React from "react";
+import "./Register.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile, faDollarSign } from "@fortawesome/free-solid-svg-icons";
 
 const Register = () => {
-    return (
-        <Modal.Dialog>
-            <Modal.Header>
-                <Modal.Title>Registro</Modal.Title>
-            </Modal.Header>
+  return (
+    <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>Registro</Modal.Title>
+      </Modal.Header>
 
-            <Modal.Body>
-                <div className="entry">
-                <p>Tipo Movimiento: </p>
-                <Form.Select aria-label="Default select example">
-                    <option>Seleccione el tipo de operación</option>
-                    <option value="1">Ingreso</option>
-                    <option value="2">Gasto</option>
-                </Form.Select>
-                </div>
-                <br />
-                <div className="entry">
-                    <p>Nombre:   </p>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text><FontAwesomeIcon icon={faFile} /> </InputGroup.Text>
-                        <FormControl aria-label="" />
-                    </InputGroup>
-                </div>
-                <div className="entry">
-                    <p>Cantidad: </p>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text><FontAwesomeIcon icon={faDollarSign} /> </InputGroup.Text>
-                        <FormControl aria-label="" />
-                    </InputGroup>
-                </div>
-
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary">Cancelar</Button>
-                <Button variant="primary">Agregar Movimiento</Button>
-            </Modal.Footer>
-        </Modal.Dialog>
-    )
-}
+      <Modal.Body>
+        <div className="entry">
+          <p>Tipo Movimiento: </p>
+          <Form.Select aria-label="Default select example">
+            <option>Seleccione el tipo de operación</option>
+            <option value="1">Ingreso</option>
+            <option value="2">Gasto</option>
+          </Form.Select>
+        </div>
+        <br />
+        <div className="entry">
+          <p>Nombre: </p>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>
+              <FontAwesomeIcon icon={faFile} />{" "}
+            </InputGroup.Text>
+            <FormControl aria-label="" />
+          </InputGroup>
+        </div>
+        <div className="entry">
+          <p>Cantidad: </p>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>
+              <FontAwesomeIcon icon={faDollarSign} />{" "}
+            </InputGroup.Text>
+            <FormControl aria-label="" />
+          </InputGroup>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary">Cancelar</Button>
+        <Button variant="primary">Agregar Movimiento</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  );
+};
 
 export default Register;
